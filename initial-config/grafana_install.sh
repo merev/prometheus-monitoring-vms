8a40060f9f97eb8b8e11d18a71ac48e1d531db1d
merev <101055426+merev@users.noreply.github.com>
2023-08-13 14:49:56 +0300

    Update grafana_install.sh

--- initial-config/grafana_install.sh
+++ initial-config/grafana_install.sh
@@ -5,7 +5,6 @@ apt-get install -y apt-transport-https
 apt-get install -y software-properties-common wget
 wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
 
-
 echo "* Add a repository for stable releases"
 echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
